7945ab5176979424a20121c4feb2f7c787f74160
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-04 22:00:27 -0700

    Improved code formatting for readability. No functional changes.

--- 5-4_accounting-scripts/melons.py
+++ 5-4_accounting-scripts/melons.py
@@ -1,13 +1,33 @@
 melon_catalogue = {
-    'Honeydew' :   {'seedless' : True, 'price' : 0.99, 'flesh_color' : None, 
-                    'weight' : None, 'rind_color' : None}, 
-    'Crenshaw' :   {'seedless' : False, 'price' : 2.00, 'flesh_color' : None, 
-                    'weight' : None, 'rind_color' : None}, 
-    'Crane' :      {'seedless' : False, 'price' : 2.50, 'flesh_color' : None, 
-                    'weight' : None, 'rind_color' : None},
-    'Casaba' :     {'seedless' : False, 'price' : 2.50, 'flesh_color' : None, 
-                    'weight' : None, 'rind_color' : None},
-    'Cantaloupe' : {'seedless' : False, 'price' : 0.99, 'flesh_color' : None, 
-                    'weight' : None, 'rind_color' : None}
+    'Honeydew' : {
+        'seedless' : True, 
+        'price' : 0.99, 
+        'flesh_color' : None, 
+        'weight' : None, 
+        'rind_color' : None}, 
+    'Crenshaw' : {
+        'seedless' : False, 
+        'price' : 2.00, 
+        'flesh_color' : None, 
+        'weight' : None, 
+        'rind_color' : None}, 
+    'Crane' : {
+        'seedless' : False, 
+        'price' : 2.50, 
+        'flesh_color' : None, 
+        'weight' : None,
+        'rind_color' : None},
+    'Casaba' : {
+        'seedless' : False, 
+        'price' : 2.50, 
+        'flesh_color' : None, 
+        'weight' : None, 
+        'rind_color' : None},
+    'Cantaloupe' : {
+        'seedless' : False, 
+        'price' : 0.99, 
+        'flesh_color' : None, 
+        'weight' : None, 
+        'rind_color' : None}
 }
 
