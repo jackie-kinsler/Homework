0ad7c0c8caa26abc761dc659e5d7f6f321542e99
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-04 21:20:05 -0700

    Initial commit

---
+++ 5-4_accounting-scripts/melons.py
@@ -0,0 +1,37 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melon_catalogue = {
+    'Honeydew' :   {'seedless' : True, 'price' : 0.99, 'flesh_color' : None, 
+                    'weight' : None, 'rind_color' : None, 'local' : None}, 
+    'Crenshaw' :   {'seedless' : False, 'price' : 2.00, 'flesh_color' : None, 
+                    'weight' : None, 'rind_color' : None, 'local' : None}, 
+    'Crane' :      {'seedless' : False, 'price' : 2.50, 'flesh_color' : None, 
+                    'weight' : None, 'rind_color' : None, 'local' : None},
+    'Casaba' :     {'seedless' : False, 'price' : 2.50, 'flesh_color' : None, 
+                    'weight' : None, 'rind_color' : None, 'local' : None},
+    'Cantaloupe' : {'seedless' : False, 'price' : 0.99, 'flesh_color' : None, 
+                    'weight' : None, 'rind_color' : None, 'local' : None}
+}
+
